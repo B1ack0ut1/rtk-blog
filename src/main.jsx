e79d1a452d2a124c12e84fadde040b3e07169615
B1ack0ut1 <42845068+B1ack0ut1@users.noreply.github.com>
2024-03-28 02:10:57 +0800

    Made SinglePostpage Component and set up some routing with react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { fetchUsers } from "./features/users/usersSlice";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 store.dispatch(fetchUsers());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
   </Provider>
 );
